aff417536e520d0d8d4fdc443917b22a880c1815
Hari Krishna Raj <hari.krishnaraj@checkpt.com>
2021-05-30 20:58:24 +0530

    Added postgres

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mailFilter',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('MAILFILTER_DB_HOST'),
+        'PORT': os.environ.get('MAILFILTER_DB_PORT'),
+        'NAME': os.environ.get('MAILFILTER_DB_NAME'),
+        'USER': os.environ.get('MAILFILTER_DB_USER'),
+        'PASSWORD': os.environ.get('MAILFILTER_DB_PASS'),
     }
 }
 
