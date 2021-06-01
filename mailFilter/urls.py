c2ac3c07f31e0fe25a3987b77d9f76646dc9a1dd
Hari Krishna Raj <hari.krishnaraj@checkpt.com>
2021-06-01 23:18:11 +0530

    Gmail Authentication

--- mailFilter/urls.py
+++ mailFilter/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('authenticate/', views.authenticate, name='authenticate'),
+    path('callback/', views.callback, name='callback')
 ]
--- mailFilter/views.py
+++ mailFilter/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from config import settings
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 # Create your views here.
 
 def home(request):
     return render(request, 'login.html')
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    'client_secret.json',
+    scopes = ['https://mail.google.com/'],
+    redirect_uri = 'http://localhost:8000/callback/')
+
+def authenticate(request):
+    auth_url, state = flow.authorization_url(prompt='consent')
+    return HttpResponseRedirect(auth_url)
+
+def callback(request):
+    return render(request, 'test.html')
