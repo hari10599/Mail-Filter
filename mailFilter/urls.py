2cdacbbccf1e4de7227c2a028a65d40732c93761
Hari Krishna <hari10599@gmail.com>
2021-06-06 19:12:38 +0530

    Added session storage

--- mailFilter/urls.py
+++ mailFilter/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('authenticate/', views.authenticate, name='authenticate'),
-    path('callback/', views.callback, name='callback')
+    path('callback/', views.callback, name='callback'),
+    path('table/', views.table, name='table'),
 ]
--- mailFilter/views.py
+++ mailFilter/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from config import settings
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
 import json
 
 flow = InstalledAppFlow.from_client_secrets_file(
@@ -11,7 +12,11 @@ flow = InstalledAppFlow.from_client_secrets_file(
     redirect_uri = json.load(open('client_secret.json'))['web']['redirect_uris'][0])
 
 def home(request):
-    return render(request, 'login.html')
+    try:
+        request.session['credentials']
+        return redirect('/table/')
+    except:
+        return render(request, 'login.html')
 
 def authenticate(request):
     auth_url, state = flow.authorization_url(prompt='consent')
@@ -19,7 +24,11 @@ def authenticate(request):
 
 def callback(request):
     flow.fetch_token(code=request.GET['code'])
-    credentials = flow.credentials
+    request.session['credentials'] = flow.credentials.token
+    return redirect('/table/')
+
+def table(request):
+    credentials = Credentials(request.session['credentials'])
     service = build('gmail', 'v1', credentials = credentials)
     api_response = service.users().getProfile(userId = "me").execute()
     print(api_response)
