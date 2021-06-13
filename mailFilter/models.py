ac8aa52766fd316da45c30ef04cb7899b9b7ac97
Hari Krishna <hari10599@gmail.com>
2021-06-13 17:44:35 +0530

    Added models

--- mailFilter/models.py
+++ mailFilter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models import JSONField
 
 # Create your models here.
+class User(models.Model):
+    userName = models.TextField(primary_key=True)
+    createdTime = models.DateTimeField()
+    lastUpdatedTime = models.DateTimeField()
+
+class Mail(models.Model):
+    userName = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    mailCount = models.IntegerField(default = 0)
+    mailBox = JSONField(default = dict)
